5f55c0fe949b6dc5e81dca4d137ce9f7d3a194ce
TOVTC <veronica.tc.to@outlook.com>
2022-03-25 01:22:56 -0400

    added function to load appointments from localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,8 @@
 var currentDay = document.querySelector("#currentDay");
 currentDay.textContent = moment().format('dddd, MMMM Do');
 
-//an array for each hour of the day
-//function to package each hour's events into an object
 //function that checks and updates the current time setInterval()
 //function that changes the background colour of the timeblock
-//function to save changes as an array when save button is clicked
 //function to cancel editing if clicking away from the field
 
 //event array
@@ -21,16 +18,32 @@ var schedAppts = {
     fivePM: []
 };
 
-$("#save-button").click(function(){
-    var hour = $(this).parent().attr("id");
-    console.log(hour);
-    schedAppts.hour = [];
-    var apptText = $("#task-description").val();
-    schedAppts.hour.push(apptText);
-    console.log(schedAppts);
-    saveAppointments();
-})
+// var schedAppts = {};
 
+//save events
+$(".saveBtn").click(function(){
+    var hour = $(this).parent().attr("id");
+    schedAppts[hour] = [];
+    var apptText = $(this).parent().find(".description").val();
+    schedAppts[hour].push(apptText);
+    saveAppointments();
+    console.log(schedAppts);
+});
+
+// upload events
 var saveAppointments = function() {
     localStorage.setItem("schedAppts", JSON.stringify(schedAppts));
 }
+
+//match the array key to the id of the row and set the text value of the task description to the value in the array
+var loadAppointments = function() {
+    schedAppts = JSON.parse(localStorage.getItem("schedAppts"));
+    Object.keys(schedAppts)
+    .forEach(function eachKey(key) { 
+        keyId = "#" + key;
+        keyVal = schedAppts[key]
+        $("#container").find(keyId).find(".description").val(keyVal);
+    });
+}
+
+loadAppointments();
