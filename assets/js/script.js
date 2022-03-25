c39287ef1bebe6e964b7a416817384e3a7127306
TOVTC <veronica.tc.to@outlook.com>
2022-03-25 03:30:16 -0400

    added some responsive class modifications

--- assets/js/script.js
+++ assets/js/script.js
@@ -57,31 +57,60 @@ var loadAppointments = function() {
 //if moment() - is less than 9am, add future class to all description textareas
 //if moment() is greater than 5pm, add past class to all description text areas
 
-// var start = moment().set("hour", 9).set("minutes", 00);
-// var end = moment().set("hour", 17).set("minutes", 00);
-// if (moment() < start) {
-//     $(".description").removeClass("past");
-//     $(".description").addClass("future");
-// }
-// if (moment() > end) {
-//     $(".description").addClass("past");
-// }
 
-// time = 9;
-// iterator = 0;
 
-// Object.keys(schedAppts)
-// .forEach(function eachKey(key, index) { 
-//     keyId = "#" + key;
-//     parseInt(index);
-//     if (index < iterator) {
-//         $("#container").find(keyId).find(".description").addClass("past")
-//     } else if (index === iterator) {
-//         $("#container").find(keyId).find(".description").addClass("present")
-//     } else if (index > iterator) {
-//         $("#container").find(keyId).find(".description").addClass("future")
-//     }
-// });
+
+//need to work out bug with iterator and when it begins to count
+var iterator = 0;
+var iterate = function() {
+    iterator = iterator + 1
+    console.log(iterator);
+}
+
+var start = moment().set("hour", 3).set("minutes", 20);
+var end = moment().set("hour", 3).set("minutes", 21);
+
+if (moment() < start) {
+    $(".description").removeClass("past");
+    $(".description").addClass("future");
+}
+if (moment() === start) {
+    var timeOfDay = setInterval(iterate, 5000);
+    var realTime = setInterval(currentTime, 5000);
+}
+if (moment() === end) {
+    clearInterval(timeOfDay);
+    clearInterval(realTime);
+    iterator = 0;
+}
+if (moment() > end) {
+    $(".description").addClass("past");
+}
+
+var currentTime = function() {
+    Object.keys(schedAppts)
+    .forEach(function eachKey(key, index) { 
+        keyId = "#" + key;
+        parseInt(index);
+        console.log(index);
+        if (index < iterator) {
+            $("#container").find(keyId).find(".description").removeClass("future")
+            $("#container").find(keyId).find(".description").removeClass("present")
+            $("#container").find(keyId).find(".description").addClass("past")
+            console.log("less than")
+        } else if (index === iterator) {
+            $("#container").find(keyId).find(".description").removeClass("future")
+            $("#container").find(keyId).find(".description").removeClass("past")
+            $("#container").find(keyId).find(".description").addClass("present")
+            console.log("equal to")
+        } else if (index > iterator) {
+            $("#container").find(keyId).find(".description").removeClass("present")
+            $("#container").find(keyId).find(".description").removeClass("past")
+            $("#container").find(keyId).find(".description").addClass("future")
+            console.log("greater than")
+        }
+    });
+}
 
 //use a set interval to increase the value of time and iterator by 1 in the background
 //if the index of the key in the schedAppts is equal to or greater than the iterator, add or remove a class
@@ -90,4 +119,5 @@ var loadAppointments = function() {
 //function that checks and updates the current time setInterval()
 //function that changes the background colour of the timeblock
 
+currentTime();
 loadAppointments();
